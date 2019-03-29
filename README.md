18bb96f3676b480d6bb7cd32952b4c3c5de2a329
freewind <nowindlee@gmail.com>
2019-03-29 23:17:24 +0800

    demo

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-TypeScript React Hello World Demo
+TypeScript React Select Demo
 =================================
+
+既可以输入又可以选择的Select组件。
 
 ```
 npm install
 npm run demo
 ```
-
-It will open page on browser automatically.
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,12 +1,24 @@
 //import * as React from 'react'
-import React from 'react'
+import React, {useState} from 'react'
+import Select from 'react-select';
+import {ValueType, OptionsType} from "react-select/lib/types";
 
-class Hello extends React.Component {
-    render() {
-        return <div>
-            <h1>Hello React</h1>
-        </div>
-    }
+type OptionType = {
+  value: string,
+  label: string
 }
 
-export default Hello
+const options: OptionsType<OptionType> = [
+  {value: 'chocolate', label: 'Chocolate'},
+  {value: 'strawberry', label: 'Strawberry'},
+  {value: 'vanilla', label: 'Vanilla'}
+];
+
+export default function Hello() {
+  const [selectedOption, setSelectedOption] = useState<ValueType<OptionType>>(null)
+  return <div>
+    <h1>Hello React Select</h1>
+    <Select value={selectedOption} onChange={setSelectedOption} options={options}/>
+    <div>{JSON.stringify(selectedOption)}</div>
+  </div>
+};
